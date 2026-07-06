d5c2799388d3619d416490dc6a65ed8c0f47de90
copi143 <copi143@outlook.com>
2026-07-07 03:12:43 +0800

    fix: chinese translate

--- src/i18n/versionHistory/zh-CN.ts
+++ src/i18n/versionHistory/zh-CN.ts
@@ -1,11 +1,11 @@
 export const versionHistory = {
   title: "版本历史",
-  subtitle: "浏览过往版本及其下载",
+  subtitle: "浏览并下载旧版",
   noVersions: "暂无可用版本。",
   downloads: {
-    mmc: "MultiMC/Prism ZIP 包",
-    server: "服务端 ZIP 包",
-    client: "原版启动器 ZIP 包",
+    mmc: "MultiMC/Prism 包",
+    server: "服务端包",
+    client: "原版启动器包",
     java17: "Java 17-{max} ZIP",
     java8: "Java 8 ZIP",
     noMmc: "此版本没有 MMC 下载。",
